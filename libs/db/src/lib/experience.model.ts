cc728b9d270b401790d9ce578a0d217e8af3cef0
Sachi Patankar <sachipatankar19@gmail.com>
2025-04-19 17:30:15 +0530

    Changed up schemas

--- libs/db/src/lib/experience.model.ts
+++ libs/db/src/lib/experience.model.ts
@@ -1,32 +1,16 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import { IAddress } from './profile.model';
 import { v4 as uuidv4 } from 'uuid';
 
 export interface IExperience extends Document {
   _id: string;
   profile_id: string;
-  user_id: string;
   company: string;
   position?: string;
   duration?: string;
-  address?: IAddress;
+  location_id?: string;
   created_at?: Date;
   updated_at?: Date;
 }
-
-const AddressSchema = new Schema<IAddress>({
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  country: { type: String, required: true },
-  postal_code: { type: String, required: true },
-  state_code: { type: String, required: true },
-  country_code: { type: String, required: true },
-  coordinates: {
-    latitude: { type: Number },
-    longitude: { type: Number },
-  },
-});
 
 const ExperienceSchema = new Schema<IExperience>(
   {
@@ -36,11 +20,10 @@ const ExperienceSchema = new Schema<IExperience>(
       default: uuidv4,
     },
     profile_id: { type: String, required: true },
-    user_id: { type: String, required: true },
     company: { type: String, required: true },
     position: { type: String },
     duration: { type: String },
-    address: AddressSchema,
+    location_id: { type: String },
   },
   {
     timestamps: {
--- libs/db/src/lib/project.model.ts
+++ libs/db/src/lib/project.model.ts
@@ -1,23 +1,32 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
-
 export interface IProject extends Document {
   _id: string;
   title: string;
   description?: string;
   link?: string;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
-const ProjectSchema = new Schema<IProject>({
-  _id: {
-    type: Schema.Types.String,
-    required: true,
-    default: uuidv4,
+const ProjectSchema = new Schema<IProject>(
+  {
+    _id: {
+      type: Schema.Types.String,
+      required: true,
+      default: uuidv4,
+    },
+    title: { type: String, required: true },
+    description: { type: String },
+    link: { type: String },
   },
-  title: { type: String, required: true },
-  description: { type: String },
-  link: { type: String },
-});
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  }
+);
 
 export const Project = mongoose.model<IProject>('Project', ProjectSchema);
